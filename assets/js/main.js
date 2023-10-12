1dab30e659268210b1e63bd3a3ca861fcf29d5ea
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-10-12 12:54:04 +0200

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,49 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 /*=============== SERVICES MODAL ===============*/
 
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', ()=>{
+    modal(i)
+    }) 
+})
+
+modalClose.forEach((mc)=>{
+    mc.addEventListener('click', ()=>{
+        modalViews.forEach((mv)=>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active work */ 
 
+const linkwork = document.querySelectorAll('.work__item')
+
+function activework(){
+    linkwork.forEach(L=> L.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkwork.forEach(L=> L.addEventListener('click', activework) )
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
